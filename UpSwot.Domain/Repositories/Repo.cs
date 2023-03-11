e6c5cc808e137e834571ea7be9b869f8fc9bb0c2
AlinaRusanova <104161655+AlinaRusanova@users.noreply.github.com>
2023-03-11 12:05:05 +0200

    added dispose for httpClient

--- UpSwot.Domain/Repositories/Repo.cs
+++ UpSwot.Domain/Repositories/Repo.cs
@@ -2,9 +2,10 @@
 
 namespace UpSwot.Domain.Repositories
 {
-    public class Repo: IRepo
+    public class Repo: IRepo, IDisposable
     {
         private static readonly HttpClient client = new HttpClient();
+        bool _disposed;
 
         public async Task<T> GetDataFromTheRickAndMortyApiResult<T>(string url)
         {
@@ -18,5 +19,22 @@ namespace UpSwot.Domain.Repositories
             var entityResponse = JsonSerializer.Deserialize<T>(responseBody, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true  });
             return entityResponse;
         }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                client.Dispose();
+
+            _disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
     }
 }
